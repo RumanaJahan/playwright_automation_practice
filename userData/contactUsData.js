b87a2e9e0cd542c43c5b313cb8bbe7e6e83a0832
Rumana Jahan <rumanaj@yahoo.com>
2025-09-24 11:37:07 +1000

    contact us data modified

--- userData/contactUsData.js
+++ userData/contactUsData.js
@@ -5,27 +5,27 @@ export const contactUsInput = {
     email: "john.doe@example.com",
     subject: "Inquiry about services",
     message: "Hello, I would like to know more about your services.",
-    filePath: "../rumautomation/userData/contactus.txt"
+    filePath: "../playwright-automation-exercise/userData/contactus.txt"
   },
   missingEmail: {
     name: "Jane Doe",
     email: "",
     subject: "Missing email case",
     message: "Testing contact form with missing email.",
-    filePath: "../rumautomation/userData/contactus.txt"
+    filePath: "../playwright-automation-exercise/userData/contactus.txt"
   },
   missingAt: {
     name: "Jane Doe",
     email: "test",
     subject: "Invalid email case",
     message: "Testing contact form with missing @ in email.",
-    filePath: "../rumautomation/userData/contactus.txt"
+    filePath: "../playwright-automation-exercise/userData/contactus.txt"
   },
   missingDomain: {
     name: "Mark Smith",
     email: "test@",
     subject: "No message provided",
     message: "Testing contact form with missing domain in email.",
-    filePath: "../rumautomation/userData/contactus.txt"
+    filePath: "../playwright-automation-exercise/userData/contactus.txt"
   }
 };
